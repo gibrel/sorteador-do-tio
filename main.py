962b3590a39eb846d9dd7e3466a0ee2fd23572d6
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2023-06-09 16:51:28 -0300

    updated layout, added bg image

--- main.py
+++ main.py
@@ -1,155 +1,187 @@
-import tkinter as tk
-from tkinter import ttk
-import tkinter.messagebox as messagebox
-import win32clipboard
-from random import sample
+from tkinter import scrolledtext, messagebox, filedialog
+from PIL import Image
+import random
+import json
+import customtkinter as ctk
+import pandas as pd
+
+# Constantes
+IMPORT_BUTTON_TEXT = "Importar"
+CLEAR_BUTTON_TEXT = "Limpar"
+EXPORT_FORMAT_DEFAULT = "-- SELECIONE --"
+EXPORT_FORMAT_OPTIONS = ["-- SELECIONE --", "JSON", "EXCEL"]
+EXPORT_BUTTON_TEXT = "Exportar"
+EXIT_BUTTON_TEXT = "Sair"
+RESUME_LABEL_TEXT = "OS IMPERIALISTAS SÃO APENAS TIGRES DE PAPEL"
+QUANTITY_LABEL_TEXT = "Quantidade:"
+PICK_BUTTON_TEXT = "Sortear"
+ERROR_TITLE = "Erro"
+ERROR_INVALID_QUANTITY = "A quantidade inserida é inválida."
+ERROR_INVALID_EXPORT_FORMAT = "Selecione um formato de exportação válido."
+JSON_FILE_TYPE = [("Arquivo JSON", "*.json")]
+EXCEL_FILE_TYPE = [("Planilha Excel", "*.xlsx")]
+
+ctk.set_default_color_theme("dark-blue")
+ctk.set_appearance_mode("system")
 
 
-class ResultWindow(tk.Toplevel):
-    def __init__(self, names):
-        super().__init__()
-        self.title("Resultados")
-        self.geometry("800x600")
-
-        self.result_text = tk.Text(self, height=10, width=30)
-        self.result_text.pack(fill=tk.BOTH, expand=True)
-
-        scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.result_text.yview)
-        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-        self.result_text.configure(yscrollcommand=scrollbar.set)
-
-        self.display_results(names)
-
-    def display_results(self, names):
-        result = "Nomes Sorteados:\n\n"
-        for i, name in enumerate(names, start=1):
-            result += f"{i}. {name}\n"
-        self.result_text.insert(tk.END, result)
-        self.result_text.config(state=tk.DISABLED)
-
-
-class Application(tk.Tk):
+class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.title("Aplicativo de Planilha de Nomes")
+        self.title("Aplicativo de Sorteio")
         self.geometry("800x600")
 
-        self.toolbar = ttk.Frame(self)
-        self.toolbar.pack(side=tk.TOP, fill=tk.X)
+        # load and create background image
+        self.bg_image = ctk.CTkImage(
+            Image.open("assets/mao-zedong.jpg"),
+            size=(1600, 900))
 
-        self.tree_frame = ttk.Frame(self)
-        self.tree_frame.pack(fill=tk.BOTH, expand=True)
+        self.background_label = ctk.CTkLabel(self, image=self.bg_image)
+        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        self.tree = ttk.Treeview(self.tree_frame)
-        self.tree["columns"] = ("ID", "Name")
-        self.tree.column("#0", width=0, stretch=tk.NO)
-        self.tree.column("ID", anchor=tk.CENTER, width=50)
-        self.tree.column("Name", anchor=tk.W, width=200)
-        self.tree.heading("ID", text="ID")
-        self.tree.heading("Name", text="Nome")
+        self.top_bar = ctk.CTkFrame(self)
+        self.top_bar.pack(side=ctk.TOP, fill=ctk.X, padx=5, pady=5)
 
-        self.tree_scrollbar = ttk.Scrollbar(self.tree_frame, orient=tk.VERTICAL, command=self.tree.yview)
-        self.tree.configure(yscrollcommand=self.tree_scrollbar.set)
+        self.import_button = ctk.CTkButton(
+            self.top_bar, text=IMPORT_BUTTON_TEXT,
+            command=self.import_names)
+        self.import_button.pack(side=ctk.LEFT, padx=5, pady=5)
 
-        self.tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        self.tree_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.clear_button = ctk.CTkButton(
+            self.top_bar,
+            text=CLEAR_BUTTON_TEXT,
+            command=self.clear_sheet)
+        self.clear_button.pack(side=ctk.LEFT, padx=5, pady=5)
 
-        self.summary_label = ttk.Label(self, text="Resumo das Importações:")
-        self.summary_label.pack(pady=5)
+        self.export_format_var = ctk.StringVar()
+        self.export_format_var.set(EXPORT_FORMAT_DEFAULT)
 
-        self.summary_text = tk.Text(self, height=3)
-        self.summary_text.pack(pady=5)
+        self.export_dropdown = ctk.CTkOptionMenu(
+            self.top_bar,
+            variable=self.export_format_var,
+            values=EXPORT_FORMAT_OPTIONS)
+        self.export_dropdown.pack(side=ctk.LEFT, padx=5, pady=5)
 
-        self.quantity_label = ttk.Label(self, text="Quantidade:")
-        self.quantity_label.pack(pady=5)
+        self.export_button = ctk.CTkButton(
+            self.top_bar,
+            text=EXPORT_BUTTON_TEXT,
+            command=self.export_results)
+        self.export_button.pack(side=ctk.LEFT, padx=5, pady=5)
 
-        self.quantity_entry = ttk.Entry(self)
-        self.quantity_entry.pack(pady=5)
+        self.exit_button = ctk.CTkButton(
+            self.top_bar,
+            text=EXIT_BUTTON_TEXT,
+            command=self.quit)
+        self.exit_button.pack(side=ctk.RIGHT, padx=5, pady=5)
 
-        get_names_button = ttk.Button(self.toolbar, text="Obter Nomes (Ctrl+V)", command=self.get_clipboard_text)
-        get_names_button.pack(side=tk.LEFT, padx=5)
+        self.center_frame = ctk.CTkFrame(self)
+        self.center_frame.pack(fill=ctk.BOTH, expand=True, padx=5, pady=5)
 
-        clear_button = ttk.Button(self.toolbar, text="Limpar Planilha", command=self.clear_sheet)
-        clear_button.pack(side=tk.LEFT, padx=5)
+        self.sheet_textbox = scrolledtext.ScrolledText(self.center_frame, width=40, height=10)
+        self.sheet_textbox.pack(side=ctk.LEFT, padx=5, pady=5, fill=ctk.BOTH, expand=True)
 
-        select_button = ttk.Button(self.toolbar, text="Sortear", command=self.select_names)
-        select_button.pack(side=tk.LEFT, padx=5)
+        self.results_textbox = scrolledtext.ScrolledText(self.center_frame, width=40, height=10)
+        self.results_textbox.pack(side=ctk.LEFT, padx=5, pady=5, fill=ctk.BOTH, expand=True)
 
-        self.bind("<Control-v>", self.on_paste)
+        self.bottom_bar = ctk.CTkFrame(self)
+        self.bottom_bar.pack(side=ctk.BOTTOM, fill=ctk.X, padx=5, pady=5)
 
-        self.current_id = 1
-        self.names = []
-        self.new_entries = 0
-        self.duplicate_entries = 0
+        self.summary_label = ctk.CTkLabel(
+            self.bottom_bar,
+            text=RESUME_LABEL_TEXT)
+        self.summary_label.pack()
 
-    def get_clipboard_text(self, event=None):
-        try:
-            win32clipboard.OpenClipboard()
-            clipboard_text = win32clipboard.GetClipboardData()
-            win32clipboard.CloseClipboard()
-            name_list = clipboard_text.split("\n")
-            if name_list:
-                self.insert_names(name_list)
-            else:
-                messagebox.showinfo("Erro", "Nenhum nome encontrado na área de transferência.")
-        except Exception as e:
-            messagebox.showinfo("Erro", f"Erro ao obter os nomes da área de transferência: {str(e)}")
+        self.bottom_elements_subframe = ctk.CTkFrame(self.bottom_bar)
+        self.bottom_elements_subframe.pack()
 
-    def insert_names(self, name_list):
-        self.clear_sheet()
-        self.new_entries = 0
-        self.duplicate_entries = 0
+        self.quantity_label = ctk.CTkLabel(
+            self.bottom_elements_subframe,
+            text=QUANTITY_LABEL_TEXT)
+        self.quantity_label.pack(side=ctk.LEFT)
 
-        for name in name_list:
+        self.quantity_entry = ctk.CTkEntry(self.bottom_elements_subframe)
+        self.quantity_entry.pack(side=ctk.LEFT)
+
+        self.pick_button = ctk.CTkButton(
+            self.bottom_elements_subframe,
+            text=PICK_BUTTON_TEXT,
+            command=self.pick_names)
+        self.pick_button.pack(side=ctk.LEFT)
+
+        self.bottom_elements_subframe.pack_configure(expand=True)
+
+    def import_names(self):
+        clipboard_text = self.clipboard_get()
+        names = clipboard_text.split("\n")
+        new_entries = 0
+        repeated_entries = 0
+
+        for name in names:
             name = name.strip()
-            if name:
-                if name not in self.names:
-                    self.names.append(name)
-                    self.tree.insert("", tk.END, values=(self.current_id, name))
-                    self.current_id += 1
-                    self.new_entries += 1
-                else:
-                    self.duplicate_entries += 1
+            if name and not any(name in entry for entry in self.sheet_textbox.get("1.0", ctk.END).split("\n")):
+                new_entries += 1
+                self.sheet_textbox.insert(ctk.END, f"ID: {new_entries}\tNome: {name}\n")
+            elif name:
+                repeated_entries += 1
 
-        self.update_summary()
+        self.summary_label.configure(
+            text=f"Novas entradas: {new_entries} | "
+                 f"Entradas repetidas: {repeated_entries} | "
+                 f"Total de nomes: {new_entries + repeated_entries}")
 
     def clear_sheet(self):
-        self.tree.delete(*self.tree.get_children())
-        self.current_id = 1
-        self.names.clear()
-        self.clear_summary()
+        self.sheet_textbox.delete('1.0', ctk.END)
+        self.results_textbox.delete('1.0', ctk.END)
+        self.summary_label.configure(text=RESUME_LABEL_TEXT)
 
-    def clear_summary(self):
-        self.summary_text.config(state=tk.NORMAL)
-        self.summary_text.delete("1.0", tk.END)
-        self.summary_text.config(state=tk.DISABLED)
-
-    def update_summary(self):
-        total_entries = len(self.names)
-        self.summary_text.config(state=tk.NORMAL)
-        self.summary_text.delete("1.0", tk.END)
-        self.summary_text.insert(tk.END, f"Total de Nomes: {total_entries}\n")
-        self.summary_text.insert(tk.END, f"Novas Entradas: {self.new_entries}\n")
-        self.summary_text.insert(tk.END, f"Entradas Duplicadas: {self.duplicate_entries}\n")
-        self.summary_text.config(state=tk.DISABLED)
-
-    def on_paste(self, event):
-        self.get_clipboard_text()
-
-    def select_names(self):
+    def pick_names(self):
         quantity = self.quantity_entry.get()
-        if quantity.isdigit():
-            quantity = int(quantity)
-            if quantity <= len(self.names):
-                selected_names = sample(self.names, quantity)
-                ResultWindow(selected_names)
-            else:
-                messagebox.showinfo("Erro", "A quantidade inserida é maior que o número de nomes na planilha.")
+        names_list = self.sheet_textbox.get("1.0", ctk.END).split("\n")
+        names_list = [name.split("\t")[1] for name in names_list if name]
+
+        if quantity.isdigit() and int(quantity) <= len(names_list):
+            results = random.sample(names_list, int(quantity))
+            self.results_textbox.delete('1.0', ctk.END)
+            self.results_textbox.insert(ctk.END, "\n".join(results))
         else:
-            messagebox.showinfo("Erro", "Digite um número válido.")
+            messagebox.showerror(ERROR_TITLE, ERROR_INVALID_QUANTITY)
+
+    def export_results(self):
+        export_format = self.export_format_var.get()
+
+        if export_format == "JSON":
+            self.export_to_json()
+        elif export_format == "EXCEL":
+            self.export_to_excel()
+        else:
+            messagebox.showerror(ERROR_TITLE, ERROR_INVALID_EXPORT_FORMAT)
+
+    def export_to_json(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".json", filetypes=JSON_FILE_TYPE)
+
+        if file_path:
+            results = self.results_textbox.get("1.0", ctk.END).splitlines()
+            results_dict = {}
+
+            for index, result in enumerate(results):
+                results_dict[index + 1] = result
+
+            with open(file_path, "w") as json_file:
+                json.dump(results_dict, json_file, indent=4)
+
+    def export_to_excel(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=EXCEL_FILE_TYPE)
+
+        if file_path:
+            results = self.results_textbox.get("1.0", ctk.END).splitlines()
+            results_list = []
+
+            for result in results:
+                results_list.append({"Resultado": result})
+
+            df = pd.DataFrame(results_list)
+            df.to_excel(file_path, index=False, engine="openpyxl")
 
 
-if __name__ == "__main__":
-    app = Application()
-    app.mainloop()
-
+app = App()
+app.mainloop()
